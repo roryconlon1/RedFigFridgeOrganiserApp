22421869dd1dc0b5470c220a30ee2a78c498c3a6
roryconlon1 <107688072+roryconlon1@users.noreply.github.com>
2022-10-06 23:15:42 +0100

    Initial commit

---
+++ src/main/java/com/redfig/example/redfigfridge/RedfigfridgeApplication.java
@@ -0,0 +1,13 @@
+package com.redfig.example.redfigfridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedfigfridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedfigfridgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redfig/example/redfigfridge/RedfigfridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redfig.example.redfigfridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedfigfridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
