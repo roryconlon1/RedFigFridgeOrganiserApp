905cff9569d2745ea1124b0d6d3b908b3579235f
roryconlon1 <107688072+roryconlon1@users.noreply.github.com>
2022-10-07 14:44:02 +0100

    Controller, see data on local host

---
+++ src/main/java/com/redfig/example/redfigfridge/controllers/FoodController.java
@@ -0,0 +1,23 @@
+package com.redfig.example.redfigfridge.controllers;
+
+import com.redfig.example.redfigfridge.models.Food;
+import com.redfig.example.redfigfridge.repositories.FoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class FoodController {
+
+    @Autowired
+    FoodRepository foodRepository;
+
+    @GetMapping(value = "/foods")
+    public ResponseEntity<List<Food>> getAllFoods(){
+        return new ResponseEntity<>(foodRepository.findAll(), HttpStatus.OK);
+    }
+}
