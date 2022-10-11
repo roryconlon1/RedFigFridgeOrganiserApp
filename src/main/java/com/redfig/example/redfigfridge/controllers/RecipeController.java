3a590b9bfb262290650d51509f5a7b49e82556d8
roryconlon1 <107688072+roryconlon1@users.noreply.github.com>
2022-10-11 14:31:48 +0100

    added put method

--- src/main/java/com/redfig/example/redfigfridge/controllers/RecipeController.java
+++ src/main/java/com/redfig/example/redfigfridge/controllers/RecipeController.java
@@ -2,6 +2,7 @@ package com.redfig.example.redfigfridge.controllers;
 
 import com.redfig.example.redfigfridge.models.Food;
 import com.redfig.example.redfigfridge.models.Recipe;
+import com.redfig.example.redfigfridge.repositories.FoodRepository;
 import com.redfig.example.redfigfridge.repositories.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,9 @@ public class RecipeController {
 
     @Autowired
     RecipeRepository recipeRepository;
+
+    @Autowired
+    FoodRepository foodRepository;
 
     @GetMapping(value = "/recipes")
     public ResponseEntity<List<Recipe>> getAllRecipes(){
@@ -31,6 +35,16 @@ public class RecipeController {
         recipeRepository.save(recipe);
         return new ResponseEntity<>(recipe, HttpStatus.CREATED);
     }
+
+    @PutMapping(value="/recipes/{recipeId}/addIngredient/{foodId}")
+    public ResponseEntity<Recipe> addIngredient(@PathVariable Long recipeId, @PathVariable  Long foodId){
+        Recipe recipe = recipeRepository.findById(recipeId).get();
+        Food food = foodRepository.findById(foodId).get();
+        recipe.addFood(food);
+        recipeRepository.save(recipe);
+        return new ResponseEntity<>(recipe, HttpStatus.OK);
+    }
+
 
     @DeleteMapping(value = "/recipes/{id}")
     public ResponseEntity<Recipe> deleteRecipe(@PathVariable Long id){
